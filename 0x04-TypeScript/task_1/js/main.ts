06e61761d46204ecebe5900db27c0a51f2123c41
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2023-01-05 05:44:30 +0200

    Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,49 +1,76 @@
-interface Student {
-    firstName: string;
-    lastName: string;
-    age: number;
-    location: string;
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  location: string;
+  yearsOfExperience?: number;
+  [propName: string]: any;
+}
+
+// Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Creating a teacher and a director
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+
+// returns a Teacher's name in this format: S. Cheung
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+console.log(printTeacher('John', 'Doe'));
+
+// class description interface
+interface classInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// constructor description interface
+interface classConstructor {
+  new (firstName: string, lastName: string): classInterface;
+}
+
+// Creating class and constructor through interfaces 
+class StudentClass implements classInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
 
-  const studentA: Student = {
-    firstName: 'Joyce',
-    lastName: 'Ramoeletsi',
-    age: 29,
-    location: 'Lesotho',
-  }
-  
-  const studentB: Student = {
-    firstName: 'Refuoe',
-    lastName: 'Makara',
-    age: 26,
-    location: 'Lesotho',
+  workOnHomework(): string {
+    return 'Currently working';
   }
 
-  const studentsList: Array<Student> = [studentA, studentB];
+  displayName(): string {
+    return this.firstName;
+  }
+}
 
-  const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
-
-const table: HTMLTableElement = document.createElement("table");
-const thead: HTMLTableSectionElement = document.createElement("thead");
-const tbody: HTMLTableSectionElement = document.createElement("tbody");
-
-const rowHead: HTMLTableRowElement = thead.insertRow(0);
-const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
-const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
-
-cell1Head.innerHTML = "firstName";
-cell2Head.innerHTML = "location";
-
-table.append(thead);
-
-studentsList.forEach((student) => {
-  const row: HTMLTableRowElement = tbody.insertRow(0);
-  const cell1: HTMLTableCellElement = row.insertCell(0);
-  const cell2: HTMLTableCellElement = row.insertCell(1);
-
-  cell1.innerHTML = student.firstName;
-  cell2.innerHTML = student.location;
-});
-
-table.append(tbody);
-body.append(table);
+// creating instance of StudentClass
+const studentClass: StudentClass = new StudentClass('Refuoe', 'Makara');
+console.log(studentClass.displayName());
+console.log(studentClass.workOnHomework());
