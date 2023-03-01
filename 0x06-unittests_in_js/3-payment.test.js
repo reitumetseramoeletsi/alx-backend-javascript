d32ab95e7e9aefffa468d9deb7e2d490c1a441fe
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2023-03-01 03:01:10 +0200

    update files

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,23 @@
+const { expect, assert } = require('chai');
+const sinon = require('sinon');
+const { spy } = require('sinon');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const utils = require('./utils');
+
+const chai = require('chai');
+const sinon = require('sinon');
+
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+
+describe('sendPaymentRequestToApi function', () => {
+  const spyUtils = sinon.spy(Utils, 'calculateNumber');
+
+  it('validate the usage of the Utils function', () => {
+    sendPaymentRequestToApi(100, 20);
+    chai.expect(spyUtils.calledOnce).to.be.true;
+    chai.expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
+    spyUtils.restore()
+  });
+});
