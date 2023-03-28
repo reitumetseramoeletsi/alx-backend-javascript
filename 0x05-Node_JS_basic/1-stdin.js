c8cabe38f2120e784ce39bb27853ce4cd98e09bc
reitumetseramoeletsi <joyceramoeletsi@gmail.com>
2023-03-28 22:02:11 +0000

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
-process.stdout.write('Welcome to Holberton School, what is your name?');
-// wait for input
-process.stdin.resume();
-// once input is entered
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
-  // check if input is coming from the terminal
-  if (process.stdin.isTTY) {
-    process.exit();
-  } else {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+process.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
 });
