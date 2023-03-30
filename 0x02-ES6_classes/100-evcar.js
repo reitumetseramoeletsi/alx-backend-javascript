2dc8ccd9dd4d0fd9b85519a5be4f7b3ac103326a
Reitumetse <joycreamoeletsi@gmail.com>
2023-03-30 23:55:39 +0200

    fix

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -6,7 +6,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  cloneCar() {
+    return new (Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color, this._range);
   }
 }
--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,13 @@
 export default class Building {
-    constructor(sqft) {
-      // Any class extending Building must implement below method
-      if (this.constructor !== Building && !this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-  
-      // Create objs
-      this._sqft = sqft;
-    }
-  
-    // Methods
-  
-    // Setters
-  
-    // Getters
-    get sqft() {
-      return this._sqft;
+  constructor(sqft) {
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
