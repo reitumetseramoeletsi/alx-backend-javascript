87ceaf74dfdfbe4ea5e13db108fa9abdd7f5c595
Reitumetse <joycreamoeletsi@gmail.com>
2023-03-31 12:05:47 +0200

    update files

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,11 @@
 export default function getFullResponseFromAPI(success) {
-    const myPromise = new Promise((resolve, reject) => {
-      if (success) {
-        resolve(
-          { status: 200, body: 'Success' },
-        );
-      } else {
-        reject(new Error('The fake API is not working currently'));
-      }
-    });
-    return myPromise;
-  }
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    }
+    reject(new Error('The fake API is not working currently'));
+  });
+}
